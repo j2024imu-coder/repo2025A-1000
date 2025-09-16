5b275502e2a1216a0adee4790f46e1f1594ec84d
j2024imu-coder <j2024imu@gmail.com>
2025-09-16 15:12:50 +0800

    Update display format

--- Program.cs
+++ Program.cs
@@ -9,14 +9,26 @@ public class DataListIoTest
 
     public static void Main()
     {
-        var resData = new byte[] { 0xAA, 0xBB, 0x031, 0x32, 0x33, 0x34, 0x35 };
+        var resData = new byte[] { 0xAA, 0xBB, 0x01, 0x031, 0x32, 0x33, 0x34, 0x35 };
+        Console.WriteLine("Check data:");
         List<byte> listData = new List<byte>(resData);
+        Console.WriteLine("Byte (Hex) | Byte (Char)");
+        Console.WriteLine("--------------------");
 
-        Console.WriteLine("Check the data.");
         foreach (byte b in listData)
         {
-            Console.Write("{0:X2}", b);
-            Console.Write("({0}) ", (char)b);
+            string hexValue = b.ToString("X2");
+            string charValue;
+            if (b >= 32 && b <= 126)  // Check for printable ASCII characters
+            {
+                charValue = Encoding.ASCII.GetString(new byte[] { b });
+            }
+            else
+            {
+                charValue = ".";
+            }
+
+            Console.WriteLine($"{hexValue,-10} | {charValue}");
         }
         Console.WriteLine();
         Console.ReadKey();
