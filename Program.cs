3c93d74a3a938bc20e041689cfae13b1dc1a6cda
j2024imu-coder <j2024imu@gmail.com>
2025-09-16 09:43:03 +0800

    DataList for IO test version 1.0.0.0

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Text;
+
+public class DataListIoTest
+{
+
+    public static void Main()
+    {
+        var resData = new byte[] { 0xAA, 0xBB, 0x031, 0x32, 0x33, 0x34, 0x35 };
+        List<byte> listData = new List<byte>(resData);
+
+        Console.WriteLine("Check the data.");
+        foreach (byte b in listData)
+        {
+            Console.Write("{0:X2}", b);
+            Console.Write("({0}) ", (char)b);
+        }
+        Console.WriteLine();
+        Console.ReadKey();
+    }
+}
+
